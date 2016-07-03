d61ba5ad0b3d241387570bd4303177ee4914bb5c
rashidaattar <rashida.attar52@gmail.com>
2016-07-04 00:43:32 +0530

    Update MainActivityFragment.java
    paid canged

--- app/src/paid/java/MainActivityFragment.java
+++ app/src/paid/java/MainActivityFragment.java
@@ -7,9 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdView;
+import android.app.ProgressDialog;
 import com.udacity.jokeandroid.JokeActivity;
 
 import java.util.concurrent.ExecutionException;
